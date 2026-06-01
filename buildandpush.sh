ddf4a30ccaf44e76bd000d0e1c984a0960a25ef2
lubshad <lubshad4u4@gmail.com>
2026-06-01 12:32:29 +0530

    buid and push script

--- buildandpush.sh
+++ buildandpush.sh
@@ -1,11 +1,15 @@
 docker buildx build \
   --platform linux/amd64 \
   --build-arg NEXT_PUBLIC_SALEOR_API_URL="https://api.exlynatural.com/graphql/" \
+  --build-arg NEXT_PUBLIC_STOREFRONT_URL=https://exlynatural.com \
+  --build-arg NEXT_PUBLIC_DEFAULT_CHANNEL=default-channel \
   -t lubshad/storefront-exlynatural:latest \
   --load .
 docker buildx build \
   --platform linux/arm64 \
   --build-arg NEXT_PUBLIC_SALEOR_API_URL="https://api.exlynatural.com/graphql/" \
+  --build-arg NEXT_PUBLIC_STOREFRONT_URL=https://exlynatural.com \
+  --build-arg NEXT_PUBLIC_DEFAULT_CHANNEL=default-channel \
   -t lubshad/storefront-exlynatural:latest-arm64 \
   --load .
 
